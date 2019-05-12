94c2d123907f50066e88d0cec36c9bdf290c9d94
Shaady Dawood <shady.youssef@gmail.com>
2019-05-12 09:44:26 +0200

    feat: 🎸 initial push
    adds {get, set, apply, query}

---
+++ README.md
@@ -0,0 +1,39 @@
+master|develop|npm
+---|---|---
+[![Build Status](https://travis-ci.org/sdawood/json-atom.svg?branch=master)](https://travis-ci.org/sdawood/json-atom)|[![Build Status](https://travis-ci.org/sdawood/json-atom.svg?branch=develop)](https://travis-ci.org/sdawood/json-atom)|[![npm version](https://badge.fury.io/js/json-atom.svg)](https://badge.fury.io/js/json-atom)
+
+# json-atom
+
+## Possible use cases
+- Redux Store Like Behavior around any JSON reference
+- Immutable deep manipulation of JSON data
+
+## Run the tests
+
+  ```
+  npm test
+  ```
+
+## FAQs
+
+## Build Targets
+Currently the following target build environments are configured for babel-preset-env plugin
+```
+ "targets": {
+   "node": 4.3,
+   "browsers": ["last 10 versions", "ie >= 7"]
+ }
+```
+In case this turns out to be not generous enough, more backward compatible babel transpilation targets would be added.
+
+## Roadmap
+
+- bigger and better
+- rule'em all
+
+## Contributing
+See [CONTRIBUTING.md](CONTRIBUTING.md)
+
+## License
+
+[MIT](LICENSE)
---
+++ src/core/jsonpath.js
@@ -0,0 +1,36 @@
+const jp = require('jsonpath/jsonpath.min');
+// const {curry} = require('it-curry'); // one-time-use curried functions! intersting but not useful here
+const {identity, get: get_, set: set_, mod} = require('shades');
+
+const {toLenses} = require('./evaluate');
+
+const value = path => modifier => data => mod(...toLenses(path))(modifier ? modifier : identity)(data);
+const get = path => data => get_(...toLenses(path))(data);
+const set = path => value => data => set_(...toLenses(path))(value)(data);
+const query = path => data => jp.query(data, path);
+
+const favorCurry = true;
+
+const $interface = {
+    value,
+    get,
+    set,
+    query
+};
+
+// const getIterface = ({curryAll = favorCurry} = {}) => ({
+//     value: curryAll ? curry(value) : value,
+//     valueFor: curryAll ? value : curry(value),
+//     get: curryAll ? curry(get) : get,
+//     getFor: curryAll ? get : curry(get),
+//     set: curryAll ? curry(set) : set,
+//     setFor: curryAll ? set : curry(set),
+//     query: curryAll ? curry(query) : query,
+//     queryFor: curryAll ? query : curry(query)
+// });
+
+module.exports = {
+    ...$interface,
+    // ...getIterface(),
+    // getIterface
+};
