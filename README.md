f1b4f67b007053767c9412e752cf460f1ed317c0
Shaady Dawood <shady.youssef@gmail.com>
2019-05-12 23:39:23 +0200

    feat(README): seed docs

--- README.md
+++ README.md
@@ -3,6 +3,47 @@ master|develop|npm
 [![Build Status](https://travis-ci.org/sdawood/json-atom.svg?branch=master)](https://travis-ci.org/sdawood/json-atom)|[![Build Status](https://travis-ci.org/sdawood/json-atom.svg?branch=develop)](https://travis-ci.org/sdawood/json-atom)|[![npm version](https://badge.fury.io/js/json-atom.svg)](https://badge.fury.io/js/json-atom)
 
 # json-atom
+```
+    const {get, set, apply} = require('json-atom');
+    const data = {
+        user: {
+            contact: {
+                home: {
+                    number: 11111
+                }, mobile: {
+                    number: 22222
+                }
+            },
+            posts: [
+                {content: 'Hello World!', likes: 10},
+                {content: 'Goodbye World!', likes: 1}
+            ]
+        }
+    };
+
+    let result = get('$.user.contact.home.number')(data);
+    // 11111
+
+    result = get('$.user.contact["home","mobile"].number')(data);
+    // {home: 11111, mobile: 22222}
+
+    result = set('$.user.contact["home","mobile"].number')(99999)(data)
+
+    // set is Immutable, data hasn't been modified, a new refernece is returned
+    // lenses powering json-atom effectively behave like HAMT
+    // They only create new objects for the nodes from root to path, the rest of the structure is shared.
+
+    // {"user":{"contact":{"home":{"number":99999},"mobile":{"number":99999}},"posts":[{"content":"Hello World!","likes":10},{"content":"Goodbye World!","likes":1}]}}
+
+    // number fields under both branches, namely 'home' AND 'mobile' are both set to value in the new object reference
+
+
+
+
+
+
+
+```
 
 ## Possible use cases
 - Redux Store Like Behavior around any JSON reference
--- src/core/jsonpath.spec.js
+++ src/core/jsonpath.spec.js
@@ -1,14 +1,48 @@
 const {get, set, value, query} = require('./jsonpath');
 
 const data = {
-    a1: {b1: {c1: {value: 'Hello World!'}}},
-    a2: {b2: {c2: {value: 'Goodbye World!'}}}
+    user: {
+        contact: {
+            home: {
+                number: 11111
+            }, mobile: {
+                number: 22222
+            }
+        },
+        posts: [
+            {content: 'Hello World!', likes: 10},
+            {content: 'Goodbye World!', likes: 1}
+        ]
+    }
 };
 
-describe('scenario: curried jsonpath interface', () => {
-    it('works: get', () => {
-        const result = get('$.a1.b1.c1.value')(data);
-        const expectedResult = data.a1.b1.c1.value;
+describe('get set mod', () => {
+    it('get simple path', () => {
+        const result = get('$.user.contact.home.number')(data);
+        const expectedResult = data.user.contact.home.number;
         expect(result).toEqual(expectedResult);
+    });
+    it('set simple path', () => {
+        const result = set('$.user.contact.home.number')(99999)(data);
+        expect(result !== data).toBeTruthy();
+    });
+
+    it('uses structural sharing, which is a typical behavior of immutable lenses', () => {
+        const result = set('$.user.contact.home.number')(99999)(data);
+        expect(result !== data).toBeTruthy();
+        result.user.contact.mobile.number = 'SHARED_REFERENCE';
+        expect(data.user.contact.mobile.number).toEqual('SHARED_REFERENCE');
+    });
+
+    it('get branching path', () => {
+        const result = get('$.user.contact["home","mobile"].number')(data);
+        const expectedResult = {home: data.user.contact.home.number, mobile: data.user.contact.mobile.number};
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('set branching path', () => {
+        const result = set('$.user.contact["home","mobile"].number')(99999)(data);
+        expect(result.user.contact.home.number).toEqual(result.user.contact.mobile.number);
+        expect(result.user.contact.home.number).toEqual(99999);
     });
 });
